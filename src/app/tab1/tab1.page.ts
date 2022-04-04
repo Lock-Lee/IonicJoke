5158b76591664ae7d9f8ef802cb4e43a48c14be3
Thirawut <49267437+Lock-Lee@users.noreply.github.com>
2022-04-04 18:00:03 +0700

    Con

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,82 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { AppserviceService } from '../appservice.service';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+  gaugeType = 'semi';
 
-  constructor() {}
+  public readTemp;
+  public readHum;
+  public readLux;
+  
+  public time_start1;
+  public time_start2;
+  public time_start3;
+  public time_end1;
+  public time_end2;
+  public time_end3;
+  public  Lux_start;
+  public  Lux_end;
+  public  temp_start;
+  public  temp_end;
+  constructor(public service: AppserviceService) {
+    
+    this.service.message((val) => {
+      if (val.topic == '/Jokeiot/Jokeiot/getSensor') {
+        this.readLux = `${val.message}`.split(',')[0];
+        this.readTemp = `${val.message}`.split(',')[1];
+        this.readHum = `${val.message}`.split(',')[2];
 
+        
+      }
+    });
+    this.service.message((val) => {
+      if (val.topic == '/Jokeiot/Jokeiot/timeall') {
+        this.time_start1 = `${val.message}`.split(',')[0];
+        this.time_end1 = `${val.message}`.split(',')[1];
+        this.time_start2 = `${val.message}`.split(',')[2];
+        this.time_end2 = `${val.message}`.split(',')[3];
+        this.time_start3 = `${val.message}`.split(',')[4];
+        this.time_end3 = `${val.message}`.split(',')[6];
+
+       
+      }
+    });
+      this.service.message((val) => {
+      if (val.topic == '/Jokeiot/Jokeiot/setall') {
+        this.Lux_start = `${val.message}`.split(',')[0];
+        this.Lux_end = `${val.message}`.split(',')[1];
+        this.temp_start = `${val.message}`.split(',')[2];
+        this.temp_end = `${val.message}`.split(',')[3];
+       
+
+       
+      }
+    });
+
+  }
+
+  ngOnInit() {
+    this.service.message((val) => {
+      if (val.topic == '/Jokeiot/Jokeiot/Evap') {
+        console.log(val);
+      }
+    });
+  }
+  public readSensor() {
+    this.service.publish(
+      `/readsensor`,
+      `1`
+    );
+  }
+  public readValue() {
+    this.service.publish(
+      `/readdata`,
+      `1`
+    );
+  }
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,131 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { AppserviceService } from '../appservice.service';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+  styleUrls: ['tab3.page.scss'],
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit {
+  public Evap: boolean = false;
+  public openlight: boolean = false;
+  
+  public dooron: boolean = false;
+  public dooroff: boolean = false;
+  public status1: boolean = false;
+  public door_status: any = false;
+  public light_status: any = false;
+  public evap_status: any = false;
+  public water_status: any = false;
+  public water: any = false;
+  public  status_mode :any =1;
+  public status_door;
+  public status_evap;
+  public status_light;
+  public status_water;
+  public status_con ="";
+  constructor(
+    public fb: AngularFireDatabase,
+    public service: AppserviceService
+  ) {
+    this.status_con  =  this.service.present();
 
-  constructor() {}
+    this.service.message((val) => {
+      if (val.topic == '/Jokeiot/Jokeiot/status') {
+        this.door_status = `${val.message}`.split(',')[0];
+        this.evap_status = `${val.message}`.split(',')[1];
+        this.light_status = `${val.message}`.split(',')[2];
+        this.status_water = `${val.message}`.split(',')[3];
+      }
+    });
+   
+    
+    this.status_con  =  service.present();
+    console.log(service.present());
+    
+    console.log(this.status_con);
+    
+  }
+  ngOnInit() {
+    this.status_con  =  this.service.present();
+    this.status_mode = 0
+   
+     
+  }
+  public onClickMode = (e) => {
+    console.log(e);
+    
+   this.status_mode = e.detail.value;
+  
+    console.log(this.status_mode);
+    
+  
+      this.service.publish(`/mode`, `${this.status_mode}`);
+  };
+  public onClickEvap = (e) => {
+    this.status_evap = e.detail.checked;
+    let x;
+    if (e.detail.checked) {
+      x = 1;
+    } else {
+      x = 0;
+    }
+    console.log(x);
 
+   
+      this.service.publish(`/Evap`, `${x}`);
+  };
+  public onClickWater = (e) => {
+    this.status_evap = e.detail.checked;
+    let x;
+    if (e.detail.checked) {
+      x = 1;
+    } else {
+      x = 0;
+    }
+    console.log(x);
+
+   
+      this.service.publish(`/pumpwater`, `${x}`);
+  };
+  public onClicklight = (e) => {
+    this.status_light = e.detail.checked;
+
+    let x;
+    if (e.detail.checked) {
+      x = 1;
+    } else {
+      x = 0;
+    }
+  
+  
+      this.service.publish(`/light`, `${x}`);
+  };
+  public onClickonDoor = (e) => {
+    console.log(e.detail.checked);
+    if (e.detail.checked === true) {
+      this.status1 = true;
+      this.dooroff = false;
+
+      this.status_door = 'on';
+      this.service.publish(`/Door`, `on`);
+     
+    }
+  };
+  public onClickoffDoor = (e) => {
+    console.log(e.detail.checked);
+    if (e.detail.checked === true) {
+      this.status1 = false;
+      this.dooron = false;
+      this.status_door = 'off';
+     
+        this.service.publish(`/Door`, `off`);
+    }
+  };
+  public readstatus() {
+    this.service.publish(`/readstatus`, `1`);
+    
+    this.status_con  =  this.service.present();
+  }
 }
